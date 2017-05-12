278caadcf4911a14b45b1a631b1275af9bcc542c
Kapeteni Polutea <polutekape@pravda.ecs.vuw.ac.nz>
2017-05-12 18:11:00 +1200

    test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var path = require('path');
 var port = process.env.PORT||8080;
 
 //connect to databa
+//var connectionString = process.env.DATABASE_URL||"postgres://polutekape:myvuw2017@depot:5432/polutekape_jdbc";
 var connectionString = process.env.DATABASE_URL||"postgres://polutekape:myvuw2017@depot:5432/polutekape_jdbc";
 var client = new pg.Client(connectionString);
 client.connect();
